4ae3ca44b9d6c62128ab9ce53147c06557d8de2c
Daniil Beltyukov <beltyukovdanil@gmail.com>
2018-10-27 18:19:24 +0300

    Code refactoring
    - Removed form element. so no need to preventDefault in js
    - Now searching elements by id
    - Some small fixes

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,14 +1,14 @@
-const SEARCH_THRESHHOLD = 1000;
+const SEARCH_THRESHOLD = 1000;
 const API_URI = '';
 const DOMAIN_ENDPOINT = API_URI + '/domain/';
-const RESULT_DIV = 'div#results';
-const SEARCH_INPUT = 'input#search-input';
+const ID_RESULTS_DIV = 'results';
+const ID_SEARCH_INPUT = 'search-input';
 
 let searchInputTime = Date.now();
 let shouldSearch = false;
 
 const searchDomain = async (domain) => {
-  const response = await fetch('http://localhost:3000' + DOMAIN_ENDPOINT + domain);
+  const response = await fetch(DOMAIN_ENDPOINT + domain);
 
   return response.json();
 };
@@ -16,9 +16,8 @@ const searchDomain = async (domain) => {
 const processQuery = async (domain) => {
   const resp = await searchDomain(domain);
 
-  console.log(resp);
-  const resultElement = document.querySelector(RESULT_DIV);
-  const searchElement = document.querySelector(SEARCH_INPUT);
+  const resultElement = document.getElementById(ID_RESULTS_DIV);
+  const searchElement = document.getElementById(ID_SEARCH_INPUT);
 
   for (let [tld, data] of Object.entries(resp)) {
     if (data.available === true) {
@@ -27,43 +26,27 @@ const processQuery = async (domain) => {
       child.innerHTML = `${searchElement.value}.${tld}`;
 
       resultElement.appendChild(child);
-      // resultElement.appendChild(document.createElement('break'));
     }
   }
   return true;
 };
 
 onload = () => {
-  document.querySelector('input#search-input').oninput = function processSearchBar(event) {
-    console.log('oninput called');
+  document.getElementById(ID_SEARCH_INPUT).addEventListener('input', (event) => {
     let eventTime = Date.now();
-    // let delta = eventTime - searchInputTime;
-
-    // console.log(delta);
-    // console.log(event.srcElement.value);
 
     searchInputTime = eventTime;
     shouldSearch = true;
-  };
-
-  document.querySelector('input#search-input').addEventListener('keydown', (event) => {
-    console.log('keydown called');
-    if (event.keyCode === 13 || event.keyCode === 10) {
-      event.preventDefault();
-      processQuery(event.srcElement.value).then((res) => console.log(res));
-      shouldSearch = false;
-    }
   });
 
   setInterval(async () => {
-    const searchBarElement = document.querySelector('input#search-input');
+    const searchBarElement = document.getElementById(ID_SEARCH_INPUT);
     let currentTime = Date.now();
 
-    if (shouldSearch && (currentTime - searchInputTime >= SEARCH_THRESHHOLD) && searchBarElement.value !== '') {
-      console.log('SEARCH NOW!!');
+    if (shouldSearch && (currentTime - searchInputTime >= SEARCH_THRESHOLD) && searchBarElement.value !== '') {
       shouldSearch = false;
       try {
-        console.log(await processQuery(searchBarElement.value));
+        await processQuery(searchBarElement.value);
       } catch (e) {
         console.error(`Failed to get data\n${e}`);
       }
